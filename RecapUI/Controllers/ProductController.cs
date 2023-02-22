e86679e982cc3d881c02bacee604e03d4ec8d236
yusufboyaci <boyacyusuf@gamil.com>
2023-02-22 23:08:53 +0300

    -----

---
+++ RecapUI/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecapUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
