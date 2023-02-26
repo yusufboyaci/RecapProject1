3347db545b1896affcc8e9ab13e60bbcc6efc418
yusufboyaci <boyacyusuf@gamil.com>
2023-02-26 21:17:29 +0300

    -----

--- Recap.Entities/Product.cs
+++ Recap.Entities/Product.cs
@@ -12,7 +12,7 @@ namespace Recap.Entities
         public Guid CategoryId { get; set; } //FK
         public string? Name { get; set; }
         public decimal UnitPrice { get; set; }
-        public string? QuantittPerUnit { get; set; }
+        public string? QuantityPerUnit { get; set; }
         public int UnitsInStock { get; set; }
         public virtual Category? Category { get; set; }
     }
---
+++ RecapUI/Migrations/20230226132953_2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecapUI.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantittPerUnit",
+                table: "products",
+                newName: "QuantityPerUnit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityPerUnit",
+                table: "products",
+                newName: "QuantittPerUnit");
+        }
+    }
+}
